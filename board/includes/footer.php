506561e0d601d0d81195909916780d21bfdc417e
Isaac <isaac@getinnotized.com>
2021-07-16 04:45:10 +0000

    new

--- board/includes/footer.php
+++ board/includes/footer.php
@@ -93,15 +93,9 @@
 <!--begin::Sticky Toolbar-->
 <ul class="sticky-toolbar nav flex-column pl-2 pr-2 pt-3 pb-3 mt-4">
 
+    
     <!--begin::Item-->
-    <li class="nav-item mb-2" data-toggle="tooltip" title="Layout Builder" data-placement="left">
-        <a class="btn btn-sm btn-icon btn-bg-light btn-icon-primary btn-hover-primary" href="../../../builder.html">
-            <i class="flaticon2-gear"></i>
-        </a>
-    </li>
-    <!--end::Item-->
-    <!--begin::Item-->
-    <li class="nav-item" id="kt_sticky_toolbar_chat_toggler" data-toggle="tooltip" title="Chat Example" data-placement="left">
+    <li class="nav-item" id="kt_sticky_toolbar_chat_toggler" data-toggle="tooltip" title="Chat" data-placement="left">
         <a class="btn btn-sm btn-icon btn-bg-light btn-icon-danger btn-hover-danger" href="#" data-toggle="modal" data-target="#kt_chat_modal">
             <i class="flaticon2-chat-1"></i>
         </a>
